cd284ba207ab276b0874cc2d13208ea03a2b68c0
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-06-08 20:27:00 +0900

    10

--- demo/src/main/java/com/atguigu/boot/MainApplication.java
+++ demo/src/main/java/com/atguigu/boot/MainApplication.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.atguigu.boot.bean.Pet;
-import com.atguigu.boot.bean.User;
-import com.atguigu.boot.bean.config.MyConfig;
-
-import ch.qos.logback.core.hook.DefaultShutdownHook;
-
 @SpringBootApplication(scanBasePackages = "com.atguigu")
 public class MainApplication {
 
@@ -24,37 +18,44 @@ public class MainApplication {
 		for (String name : names) {
 			System.out.println(name);
 		}
+//		
+//		// 3.从容器中获取组件
+//		Pet tom01 = run.getBean(Pet.class);
+//		Pet tom02 = run.getBean(Pet.class);
+//		System.out.println("组件="+(tom01==tom02));
+//		
+//		
+//		// 4. com.atguigu.boot.bean.config.MyConfig$$SpringCGLIB$$0@7af00bdd
+//		MyConfig bean = run.getBean(MyConfig.class);
+//		System.out.println(bean);
+//		
+//		
+//		// 如果@Configuration(proxyBeanMethods = true) 代理对象调用方法。SpringBoot总会检查这个组件是否在容器中。
+//		// 保持组件单实例
+//		User user = bean.user01();
+//		User user1 = bean.user01();
+//		System.out.println("当proxyBeanMethods为TRUE时："+(user==user1));//true
+//		//System.out.println("当proxyBeanMethods为FALSE时："+(user==user1));//false
+//		
+//		User user01 = run.getBean("user01",User.class);
+//		Pet tom = run.getBean("tom",Pet.class);
+//		
+//		System.out.println("当proxyBeanMethods为TRUE时： 用户的宠物:" + (user01.getPet() == tom));//true
+//		//System.out.println("当proxyBeanMethods为FALSE时：  用户的宠物:" + (user01.getPet() == tom));//false
+//	
+//		// 5.获取组件
+//		System.out.println("=====");
+//		for (String name : run.getBeanNamesForType(User.class)) {
+//			System.out.println(name);
+//		}
+//		System.out.println(run.getBean(DefaultShutdownHook.class));
 		
-		// 3.从容器中获取组件
-		Pet tom01 = run.getBean(Pet.class);
-		Pet tom02 = run.getBean(Pet.class);
-		System.out.println("组件="+(tom01==tom02));
 		
+		boolean tom = run.containsBean("tom");
+		System.out.println("容器中Tom组件:"+tom);//false
 		
-		// 4. com.atguigu.boot.bean.config.MyConfig$$SpringCGLIB$$0@7af00bdd
-		MyConfig bean = run.getBean(MyConfig.class);
-		System.out.println(bean);
-		
-		
-		// 如果@Configuration(proxyBeanMethods = true) 代理对象调用方法。SpringBoot总会检查这个组件是否在容器中。
-		// 保持组件单实例
-		User user = bean.user01();
-		User user1 = bean.user01();
-		System.out.println("当proxyBeanMethods为TRUE时："+(user==user1));//true
-		//System.out.println("当proxyBeanMethods为FALSE时："+(user==user1));//false
-		
-		User user01 = run.getBean("user01",User.class);
-		Pet tom = run.getBean("tom",Pet.class);
-		
-		System.out.println("当proxyBeanMethods为TRUE时： 用户的宠物:" + (user01.getPet() == tom));//true
-		//System.out.println("当proxyBeanMethods为FALSE时：  用户的宠物:" + (user01.getPet() == tom));//false
-	
-		// 5.获取组件
-		System.out.println("=====");
-		for (String name : run.getBeanNamesForType(User.class)) {
-			System.out.println(name);
-		}
-		System.out.println(run.getBean(DefaultShutdownHook.class));
+		boolean user01 = run.containsBean("user01");
+		System.out.println("容器中user01组件:"+user01);//false
 	}
 
 }
--- demo/src/main/java/com/atguigu/boot/bean/config/MyConfig.java
+++ demo/src/main/java/com/atguigu/boot/bean/config/MyConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.boot.bean.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -8,6 +9,7 @@ import com.atguigu.boot.bean.Pet;
 import com.atguigu.boot.bean.User;
 
 import ch.qos.logback.core.hook.DefaultShutdownHook;
+
 /**
  * 1.配置类里面使用@Bean标在方法上给容器注册组件，默认也是单补全的。
  * 2.配置类本身也是组件
@@ -28,25 +30,25 @@ import ch.qos.logback.core.hook.DefaultShutdownHook;
  *
  */
 
-
 // 写在容器类的组件里就可以.比如@Component、@Controller、@Service、@Repository
-@Import({User.class,DefaultShutdownHook.class})
+@Import({ User.class, DefaultShutdownHook.class })
 @Configuration(proxyBeanMethods = false) //告诉SPRINGBOOT 这是一个配置类 == 配置文件beans.XML
 public class MyConfig {
 
-	
 	/**
 	 * 外部无论对配置类中的这个组件注册方法调用多少次，获取的伏特加不急之前注册容器中的单实例对象
 	 * 
 	 * */
+
+	@ConditionalOnBean(name = "tom")
 	@Bean //给容器中添加组件，以方法名作为组件的id,返回类型就是组件类型，反回的值，就是组件在容器中的实例
 	public User user01() {
 		User zhangsan = new User("zhangsan", 18);
 		zhangsan.setPet(tomcatPet());
 		return zhangsan;
 	}
-	
-	@Bean("tom")
+
+	//@Bean("tom")
 	public Pet tomcatPet() {
 		return new Pet("tom");
 	}
